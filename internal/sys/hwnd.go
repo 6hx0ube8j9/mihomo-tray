e8f4a7eab19420906110469b7cf3e60b5e3a1676
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-08-28 20:39:25 +0800

    Update hwnd.go

--- internal/sys/hwnd.go
+++ internal/sys/hwnd.go
@@ -11,27 +11,6 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-var (
-	u32 = windows.NewLazySystemDLL("user32.dll")
-	k32 = windows.NewLazySystemDLL("kernel32.dll")
-
-	procEnumWindows        = u32.NewProc("EnumWindows")
-	procGetClassName       = u32.NewProc("GetClassNameW")
-	procIsWindowVisible    = u32.NewProc("IsWindowVisible")
-	procGetWindowThread    = u32.NewProc("GetWindowThreadProcessId")
-	procGetWindowText      = u32.NewProc("GetWindowTextW")
-	procSetWindowPos       = u32.NewProc("SetWindowPos")
-	procShowWindow         = u32.NewProc("ShowWindow")
-	procSetForeground      = u32.NewProc("SetForegroundWindow")
-	procBringToTop         = u32.NewProc("BringWindowToTop")
-	procGetForeground      = u32.NewProc("GetForegroundWindow")
-	procAttachThread       = u32.NewProc("AttachThreadInput")
-	procGetCurrentThread   = k32.NewProc("GetCurrentThreadId")
-	procGetSystemMetrics   = u32.NewProc("GetSystemMetrics")
-	procSwitchToThisWindow = u32.NewProc("SwitchToThisWindow")
-	procSystemParametersInfo = u32.NewProc("SystemParametersInfoW")
-)
-
 const (
 	SW_RESTORE     = 9
 	SWP_NOSIZE     = 0x0001
@@ -43,10 +22,8 @@ const (
 var cachedWebUIHwnd atomic.Uintptr
 
 func init() {
-	procSetContext := u32.NewProc("SetProcessDpiAwarenessContext")
-	if _, _, err := procSetContext.Call(uintptr(0xfffffffc)); err != nil && uint32(err.(syscall.Errno)) != 0 {
-		procSetAware := u32.NewProc("SetProcessDPIAware")
-		_, _, _ = procSetAware.Call()
+	if _, _, err := procSetProcessDpiContext.Call(uintptr(0xfffffffc)); err != nil && uint32(err.(syscall.Errno)) != 0 {
+		_, _, _ = procSetProcessDPIAware.Call()
 	}
 }
 
@@ -60,7 +37,7 @@ func GetIdealWindowBounds() (winW, winH, winX, winY int) {
 
 	var workArea [4]int32
 	ret, _, _ := procSystemParametersInfo.Call(0x0030, 0, uintptr(unsafe.Pointer(&workArea[0])), 0)
-	
+
 	usableW, usableH := scrW, scrH
 	if ret != 0 {
 		usableW = int(workArea[2] - workArea[0])
@@ -91,16 +68,16 @@ func GetIdealWindowBounds() (winW, winH, winX, winY int) {
 
 	case usableW >= 1920:
 		winW, winH = 1280, 800
-		
+
 	case usableW >= 1440:
-		winW, winH = 1150, 720	
-		
+		winW, winH = 1150, 720
+
 	case usableW <= 1280:
 		winW = int(w * 0.92)
 		winH = int(h * 0.88)
 		if winW < 800 { winW = 800 }
 		if winH < 600 { winH = 600 }
-		
+
 	default:
 		winW = int(w * 0.82)
 		winH = int(h * 0.80)
