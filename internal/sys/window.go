b70faa53fde9386c6a4392e21f1d74108a4dcf13
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-03 17:55:33 +0800

    Update window.go

--- internal/sys/window.go
+++ internal/sys/window.go
@@ -215,9 +215,3 @@ func IsWindowVisible(hwnd uintptr) bool {
 	vis, _, _ := procIsWindowVisible.Call(hwnd)
 	return vis != 0
 }
-
-func IsSystemShuttingDown() bool {
-	const SM_SHUTTINGDOWN = 0x2000
-	r, _, _ := procGetSystemMetrics.Call(SM_SHUTTINGDOWN)
-	return r != 0
-}
