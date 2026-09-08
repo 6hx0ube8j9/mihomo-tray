c020df3f6cdabbdefe62a3a150b1a93d7e1822b8
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-08 19:02:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -179,7 +179,7 @@ func main() {
 	}
 
 	cfgMgr := config.NewManager(baseDir, exePath)
-	cfgMgr.EnsureDefault()
+	cfgMgr.LoadAndInitMemory()
 	syncLogLevel(cfgMgr)
 
 	slog.Info("程序启动", "PID", os.Getpid(), "工作目录", baseDir)
