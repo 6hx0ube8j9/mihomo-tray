0bc6350c577069a7399c1b2627d8f4ad02446e01
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-11 16:54:12 +0800

    Update app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -335,27 +335,25 @@ func (a *Application) handleUICommand(ctx context.Context, cmd ui.UICommand) {
 
     case "ToggleTun":
 		enable := cmd.Payload == "true"
-
 		if enable && !sys.IsAdmin() {
-			msg := "虚拟网卡 (TUN) 路由功能需要接管系统网络，必须以管理员身份运行。\n\n是否立即重启并授权？"
-			if sys.ShowElevationPrompt("权限不足", msg) {
-				slog.Info("请求提权启动以开启 TUN 模式")
-				err := sys.RunAsAdmin(a.Cfg.ExePath(), a.Cfg.BaseDir(), "--enable-tun", "--restarting")
+			slog.Info("普通权限请求开启 TUN，直接发起 UAC 提权")
+			err := sys.RunAsAdmin(a.Cfg.ExePath(), a.Cfg.BaseDir(), "--enable-tun", "--restarting")
 
-				if sys.IsUserCancelled(err) {
-					slog.Info("用户取消了 UAC 提权，保持当前会话")
-				} else if err == nil {
-					slog.Info("提权请求已下发，当前普通进程退出")
-					os.Exit(0)
-				} else {
-					slog.Error("提权失败", "err", err)
-				}
+			if sys.IsUserCancelled(err) {
+				slog.Info("用户取消了 UAC 提权，保持当前会话")
+			} else if err == nil {
+				slog.Info("提权请求已下发，当前普通进程退出")
+				os.Exit(0)
+			} else {
+				slog.Error("提权失败", "err", err)
 			}
+			
 			a.pushUIState()
 			return
 		}
 
 		slog.Info("切换 TUN 模式", "enable", enable)
+		
 		a.Cfg.Set("tun", strconv.FormatBool(enable))
 		if enable {
 			a.State.SetTunRequestedTime(time.Now())
