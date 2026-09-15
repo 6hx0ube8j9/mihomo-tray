9d8a2ec1927a6ee3b737ef354423329f0f65f984
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-15 09:26:40 +0800

    Update remote.go

--- internal/config/remote.go
+++ internal/config/remote.go
@@ -64,15 +64,14 @@ func (m *Manager) FetchRemoteProfile(subURL string, proxyPort string) (*FetchRes
 		Transport: transport,
 	}
 
-	req, err := http.NewRequest("GET", subURL, nil)
+    req, err := http.NewRequest("GET", subURL, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	req.Header.Set("User-Agent", "clash-verge/v1.7.7 clash-meta")
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Accept-Encoding", "gzip, deflate") 
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
 
 	resp, err := client.Do(req)
 	
