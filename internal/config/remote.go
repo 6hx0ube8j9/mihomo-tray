4af3bae0497765cd20ce4540a848d5d28421e9ef
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-15 09:06:32 +0800

    Update remote.go

--- internal/config/remote.go
+++ internal/config/remote.go
@@ -70,8 +70,13 @@ func (m *Manager) FetchRemoteProfile(subURL string, proxyPort string) (*FetchRes
 	}
 	
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 ClashforWindows/0.20.39 mihomo/1.18.0 clash-meta")
+	
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Set("Cache-Control", "no-cache")
 
 	resp, err := client.Do(req)
+	
 	if err != nil {
 		return nil, err
 	}
