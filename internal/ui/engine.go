2af71646e2090a1a5286eb5f33ebcd3ea0d5c21a
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-17 16:58:44 +0800

    Update engine.go

--- internal/ui/engine.go
+++ internal/ui/engine.go
@@ -134,7 +134,7 @@ func (e *UIEngine) listenState() {
 					e.ni.SetIcon(e.icons[state.IconState])
 				}
 				e.updateTrayState(state)
-				e.RefreshPanelData(state.Profiles)
+				e.RefreshPanelData(state.ProfileItems)
 			})
 		}
 	}
