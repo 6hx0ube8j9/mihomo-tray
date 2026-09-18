d09619e442000d12f94a741bd13f090637a2c2fa
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-18 18:36:11 +0800

    Update tray.go

--- internal/ui/tray.go
+++ internal/ui/tray.go
@@ -83,7 +83,8 @@ func (e *UIEngine) updateTrayState(state UIState) {
 	
 	e.addSeparator()
 	e.addAction("退出程序", func() { 
-		e.sendCommand("ExitApp", "")
+		e.sendCommand("ExitApp", "") 
+		e.app.Synchronize(func() { e.mw.Close() }) 
 	})
 }
 
