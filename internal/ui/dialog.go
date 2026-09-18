20e8b9b079579fd1f26ae609a55e202eaea38f5c
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-18 10:07:09 +0800

    Update dialog.go

--- internal/ui/dialog.go
+++ internal/ui/dialog.go
@@ -1,119 +1,66 @@
 package ui
 
 import (
-	"unsafe"
-	"golang.org/x/sys/windows"
+	"github.com/tailscale/walk"
 )
 
-const (
-	MB_OK            = 0x00000000
-	MB_OKCANCEL      = 0x00000001
-	MB_ICONWARNING   = 0x00000030
-	MB_TOPMOST       = 0x00040000
-	MB_SETFOREGROUND = 0x00010000
-)
-
-var (
-	dlgModComdlg32          = windows.NewLazySystemDLL("comdlg32.dll")
-	dlgProcGetOpenFileNameW = dlgModComdlg32.NewProc("GetOpenFileNameW")
-)
-
-func syncExec(f func()) {
-	if globalUIEngine == nil || globalUIEngine.app == nil {
-		f()
-		return
-	}
-	done := make(chan struct{})
-	globalUIEngine.app.Synchronize(func() {
-		f()
-		close(done)
-	})
-	<-done
-}
-
-func getSafeOwnerHWND() windows.HWND {
+func getOwner() walk.Form {
 	if globalUIEngine != nil {
 		if globalUIEngine.panelWindow != nil && globalUIEngine.panelWindow.Visible() {
-			return windows.HWND(globalUIEngine.panelWindow.Handle())
+			return globalUIEngine.panelWindow
+		}
+		if globalUIEngine.mw != nil {
+			return globalUIEngine.mw
 		}
 	}
-	return 0
-}
-
-type OPENFILENAMEW struct {
-	LStructSize       uint32
-	HwndOwner         windows.HWND
-	HInstance         windows.Handle
-	LpstrFilter       *uint16
-	LpstrCustomFilter *uint16
-	NMaxCustFilter    uint32
-	NFilterIndex      uint32
-	LpstrFile         *uint16
-	NMaxFile          uint32
-	LpstrFileTitle    *uint16
-	NMaxFileTitle     uint32
-	LpstrInitialDir   *uint16
-	LpstrTitle        *uint16
-	Flags             uint32
-	NFileOffset       uint16
-	NFileExtension    uint16
-	LpstrDefExt       *uint16
-	LCustData         uintptr
-	LpfnHook          uintptr
-	LpTemplateName    *uint16
-	PvReserved        uintptr
-	DwReserved        uint32
-	FlagsEx           uint32
+	return nil
 }
 
 func OpenYAMLFileDialog() (string, bool) {
-	var path string
-	var accepted bool
-
-	syncExec(func() {
-		var ofn OPENFILENAMEW
-		ofn.LStructSize = uint32(unsafe.Sizeof(ofn))
-
-		filterStr := "YAML 配置文件 (*.yaml;*.yml)\x00*.yaml;*.yml\x00所有文件 (*.*)\x00*.*\x00\x00"
-		filter, _ := windows.UTF16PtrFromString(filterStr)
-		ofn.LpstrFilter = filter
-
-		buf := make([]uint16, windows.MAX_PATH)
-		ofn.LpstrFile = &buf[0]
-		ofn.NMaxFile = windows.MAX_PATH
-
-		title, _ := windows.UTF16PtrFromString("选择本地 YAML 配置文件")
-		ofn.LpstrTitle = title
-
-		ofn.Flags = 0x00001000 | 0x00000008 | 0x00000004
-		ofn.HwndOwner = getSafeOwnerHWND()
-
-		ret, _, _ := dlgProcGetOpenFileNameW.Call(uintptr(unsafe.Pointer(&ofn)))
-		if ret != 0 {
-			path = windows.UTF16ToString(buf)
-			accepted = true
-		}
+	if globalUIEngine == nil || globalUIEngine.app == nil {
+		return "", false
+	}
+	
+	type fileResult struct {
+		Path string
+		OK   bool
+	}
+	resultCh := make(chan fileResult)
+	
+	globalUIEngine.app.Synchronize(func() {
+		dlg := new(walk.FileDialog)
+		dlg.Title = "选择本地 YAML 配置文件"
+		dlg.Filter = "YAML 配置文件 (*.yaml;*.yml)|*.yaml;*.yml|所有文件 (*.*)|*.*"
+		
+		ok, _ := dlg.ShowOpen(getOwner())
+		resultCh <- fileResult{Path: dlg.FilePath, OK: ok}
 	})
-	return path, accepted
+	
+	res := <-resultCh
+	return res.Path, res.OK
 }
 
 func ShowErrorMessage(title, message string) {
-	syncExec(func() {
-		titlePtr, _ := windows.UTF16PtrFromString(title)
-		msgPtr, _ := windows.UTF16PtrFromString(message)
-		const flags = MB_OK | MB_ICONWARNING | MB_TOPMOST | MB_SETFOREGROUND
-		_, _ = windows.MessageBox(getSafeOwnerHWND(), msgPtr, titlePtr, uint32(flags))
+	if globalUIEngine == nil || globalUIEngine.app == nil {
+		return
+	}
+	
+	globalUIEngine.app.Synchronize(func() {
+		walk.MsgBox(getOwner(), title, message, walk.MsgBoxIconWarning)
 	})
 }
 
 func ShowConfirmMessage(title, message string) bool {
-	var ret int32
-	syncExec(func() {
-		titlePtr, _ := windows.UTF16PtrFromString(title)
-		msgPtr, _ := windows.UTF16PtrFromString(message)
-		const flags = MB_OKCANCEL | MB_ICONWARNING | MB_TOPMOST | MB_SETFOREGROUND
-		retVal, _ := windows.MessageBox(getSafeOwnerHWND(), msgPtr, titlePtr, uint32(flags))
-		ret = int32(retVal)
+	if globalUIEngine == nil || globalUIEngine.app == nil {
+		return false
+	}
+	
+	resultCh := make(chan int)
+	
+	globalUIEngine.app.Synchronize(func() {
+		res := walk.MsgBox(getOwner(), title, message, walk.MsgBoxIconQuestion|walk.MsgBoxOKCancel)
+		resultCh <- res
 	})
-	return ret == 1
+	
+	return <-resultCh == walk.DlgCmdOK
 }
