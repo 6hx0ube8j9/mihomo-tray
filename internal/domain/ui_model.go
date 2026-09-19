152f09e65149339845e539ed56fed3f0745eae59
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-19 17:10:29 +0800

    Create ui_model.go

---
+++ internal/domain/ui_model.go
@@ -0,0 +1,27 @@
+package domain
+
+type UICommand struct {
+	Action  string
+	Payload string
+}
+
+type UIProfileItem struct {
+	Name       string
+	Path       string
+	IsActive   bool
+	IsRemote   bool
+	Interval   int
+	LastUpdate string
+}
+
+type UIState struct {
+	IconState     int
+	IsTun         bool
+	IsProxy       bool
+	Mode          string
+	AutoStart     bool
+	IsAdmin       bool
+	RunAsAdmin    bool
+	ProfileItems  []UIProfileItem
+	CanAddProfile bool
+}
