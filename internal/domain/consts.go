3f9e5d4c85a5a52cc846b213eb6a910225885b44
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-19 17:07:08 +0800

    Create consts.go

---
+++ internal/domain/consts.go
@@ -0,0 +1,28 @@
+package domain
+
+type AppPhase int32
+
+const (
+	PhaseInitializing AppPhase = iota
+	PhaseRunning
+	PhaseExiting
+)
+
+type KernelEvent int
+
+const (
+	EventKernelReady KernelEvent = iota
+	EventKernelExit
+)
+
+const (
+	DefaultAutostart          = "false"
+	DefaultProxy              = "false"
+	DefaultTun                = "false"
+	DefaultMode               = "rule"
+	DefaultMixedPort          = "7890"
+	DefaultExternalController = "127.0.0.1:9090"
+	DefaultSecret             = ""
+	DefaultExternalUI         = "ui"
+	DefaultExternalUIURL      = "https://github.com/Zephyruso/zashboard/releases/latest/download/dist.zip"
+)
