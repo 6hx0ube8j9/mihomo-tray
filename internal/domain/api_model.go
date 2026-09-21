d25b6652aeec0e6d8286837f1610abe97d1d9f69
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-21 21:22:40 +0800

    Update api_model.go

--- internal/domain/api_model.go
+++ internal/domain/api_model.go
@@ -2,6 +2,7 @@ package domain
 
 type KernelStatus struct {
 	Mode string `json:"mode"`
+	AllowLan bool   `json:"allow-lan"`
 	Tun  struct {
 		Enable bool   `json:"enable"`
 		Device string `json:"device"`
