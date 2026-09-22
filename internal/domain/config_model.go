a1082169adee6763833a91beca2121424d81c0a6
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-22 21:50:45 +0800

    Update config_model.go

--- internal/domain/config_model.go
+++ internal/domain/config_model.go
@@ -5,60 +5,11 @@ import (
 	"time"
 )
 
-type TrayConfig struct {
-	General  GeneralConfig  `json:"general"`
-	Config   KernelConfig   `json:"config"`
-	Profiles ProfileManager `json:"profiles"`
-}
-
-type GeneralConfig struct {
-	Autostart     bool   `json:"autostart"`
-	RunAsAdmin    bool   `json:"run_as_admin"`
-	SystemBrowser bool   `json:"system_browser"`
-	TrayLogLevel  string `json:"tray_log_level"`
-	SystemProxy   bool   `json:"system_proxy"`
-}
-
-type KernelConfig struct {
-	MixedPort *int `json:"mixed-port,omitempty"`
-	Port      *int `json:"port,omitempty"`
-	SocksPort *int `json:"socks-port,omitempty"`
-
-	Mode          string `json:"mode"`
-	LogLevel      string `json:"log-level"`
-	AllowLan      bool   `json:"allow-lan"`
-	UnifiedDelay  bool   `json:"unified-delay"`
-	TcpConcurrent bool   `json:"tcp-concurrent"`
-
-	ExternalController     string     `json:"external-controller,omitempty"`
-	Secret                 string     `json:"secret,omitempty"`
-	ExternalUI             string     `json:"external-ui,omitempty"`
-	ExternalUIURL          string     `json:"external-ui-url,omitempty"`
-	ExternalUIName         string     `json:"external-ui-name,omitempty"`
-	ExternalControllerPipe string     `json:"external-controller-pipe"`
-	ExternalControllerCors CorsConfig `json:"external-controller-cors"`
-
-	Tun TunConfig `json:"tun"`
-}
-
-type CorsConfig struct {
-	AllowPrivateNetwork bool     `json:"allow-private-network"`
-	AllowOrigins        []string `json:"allow-origins"`
-}
-
-type TunConfig struct {
-	Enable bool `json:"enable"`
-}
-
-type ProfileManager struct {
-	Active string        `json:"active"`
-	Items  []ProfileItem `json:"items"`
-}
-
 type ProfileItem struct {
 	Name       string `json:"name"`
 	Path       string `json:"path"`
 	URL        string `json:"url,omitempty"`
+	AutoUpdate bool   `json:"auto_update,omitempty"`
 	Interval   int    `json:"interval,omitempty"`
 	LastUpdate int64  `json:"last_update,omitempty"`
 	Upload     int64  `json:"upload,omitempty"`
@@ -67,19 +18,40 @@ type ProfileItem struct {
 	Expire     int64  `json:"expire,omitempty"`
 }
 
+type TrayConfig struct {
+	Autostart        string        `json:"autostart"`
+	RunAsAdmin       string        `json:"run_as_admin"`
+	Mode             string        `json:"mode"`
+	Proxy            string        `json:"proxy"`
+	Tun              string        `json:"tun"`
+	TrayLogLevel     string        `json:"tray_log_level"`
+	Active           string        `json:"active"`
+	UseSystemBrowser string        `json:"use_system_browser,omitempty"`
+	AllowLan         string        `json:"allow_lan,omitempty"`
+	Items            []ProfileItem `json:"items"`
+}
+
+type FetchResult struct {
+	TempPath string
+	Upload   int64
+	Download int64
+	Total    int64
+	Expire   int64
+}
+
 func (p *ProfileItem) FormatLastUpdateText() string {
-	if p.LastUpdate == 0 {
-		return "从未更新"
-	}
-	diff := time.Since(time.Unix(p.LastUpdate, 0))
-	if diff.Hours() > 24 {
-		return fmt.Sprintf("%d 天前", int(diff.Hours()/24))
-	} else if diff.Hours() > 1 {
-		return fmt.Sprintf("%d 小时前", int(diff.Hours()))
-	} else if diff.Minutes() > 1 {
-		return fmt.Sprintf("%d 分钟前", int(diff.Minutes()))
-	}
-	return "刚刚"
+    if p.LastUpdate == 0 {
+        return "从未更新"
+    }
+    diff := time.Since(time.Unix(p.LastUpdate, 0))
+    if diff.Hours() > 24 {
+        return fmt.Sprintf("%d 天前", int(diff.Hours()/24))
+    } else if diff.Hours() > 1 {
+        return fmt.Sprintf("%d 小时前", int(diff.Hours()))
+    } else if diff.Minutes() > 1 {
+        return fmt.Sprintf("%d 分钟前", int(diff.Minutes()))
+    }
+    return "刚刚"
 }
 
 func (p *ProfileItem) IsUpdateDue() bool {
