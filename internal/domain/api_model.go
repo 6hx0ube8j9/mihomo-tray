277fa321c6012140fb98cd5a3137f9c2a6530677
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-24 21:00:17 +0800

    Update api_model.go

--- internal/domain/api_model.go
+++ internal/domain/api_model.go
@@ -1,9 +1,14 @@
 package domain
 
 type KernelStatus struct {
-	Mode string `json:"mode"`
-	AllowLan bool   `json:"allow-lan"`
-	Tun  struct {
+	Mode         string `json:"mode"`
+	LogLevel     string `json:"log-level"`
+	AllowLan     bool   `json:"allow-lan"`
+	UnifiedDelay bool   `json:"unified-delay"`
+	MixedPort    int    `json:"mixed-port"`
+	Port         int    `json:"port"`
+	SocksPort    int    `json:"socks-port"`
+	Tun          struct {
 		Enable bool   `json:"enable"`
 		Device string `json:"device"`
 	} `json:"tun"`
